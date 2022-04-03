cdecd6cd141229b6d4d4d1f108e834d4a355076f
johnhoangsoftware <johnhoangsoftware@gmail.com>
2022-04-03 18:08:26 +0700

    feat: add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
-const handlebars = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 //http Logger
 app.use(morgan('combined'))
 
 // Template enginer
-//app.engine('handlebars', handlebars())
-//app.set('view engine', 'handlebars')
-//app.set('views', path.join(__dirname, 'resources/views'))
+app.engine('hbs', engine({ extname: '.hbs', defaultLayout: "main" }));
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
